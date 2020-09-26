fc49cbf69a74f16b48aa555b09a7a1f0584736bc
swapnil <connect.swapnil02@gmail.com>
2020-09-26 22:56:49 +0530

    ipl points tablr using mock api and c#

---
+++ Iplpointtable/PointTable.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PointTable.aspx.cs" Inherits="PointTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="point_list" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="team_name" HeaderText="Team Name" />
+                <asp:BoundField DataField="Played" HeaderText="Played" />
+                <asp:BoundField DataField="Won" HeaderText="Won" />
+                <asp:BoundField DataField="Lost" HeaderText="Lost" />
+            </Columns>
+
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Iplpointtable/PointTable.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class PointTable : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string baseApiurl = "https://demo7531234.mockable.io/";
+
+        System.Net.WebClient client = new System.Net.WebClient();
+        client.Headers.Add("content-type", "application/json");
+        client.Encoding = Encoding.UTF8;
+        string result = client.DownloadString(baseApiurl + "iplpointtable");
+        PointTableResponse pointTableResponse = ((new JavaScriptSerializer()).Deserialize<PointTableResponse>(result));
+        this.point_list.DataSource = pointTableResponse.result;
+        this.point_list.DataBind();
+    }
+
+    public class PointTableResponse
+    {
+        public List<Team> result { get; set; }
+    }
+    public class Team
+    {
+        public string team_name{ get; set; }
+        public string Played{ get; set; }
+        public string Won{ get; set; }
+        public string Lost{ get; set; }
+    }
+}
